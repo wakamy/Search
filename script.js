64191695e5211a6fb7202bd3f3c7644297213845
wakamy <89887486+wakamy@users.noreply.github.com>
2021-10-24 23:44:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var list = {
+    'ts1' : {'fast':'1001','second':'1001','code':'1'},
+    'ts2' : {'fast':'1002','second':'1002','code':'2'},
+    'ts3' : {'fast':'1003','second':'1003','code':'3'},
+    'ts4' : {'fast':'1004','second':'1004','code':'4'},
+    'ts5' : {'fast':'1005','second':'1005','code':'5'},
+    'ts6' : {'fast':'1006','second':'1006','code':'6'},
+    'ts7' : {'fast':'1007','second':'1007','code':'7'},
+    'ts8' : {'fast':'1008','second':'1008','code':'8'},
+    'ts9' : {'fast':'1009','second':'1009','code':'9'},
+    'ts10' : {'fast':'1010','second':'1010','code':'10'},
+}
